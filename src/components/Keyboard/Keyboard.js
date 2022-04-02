310f227d64d9b576327802c77d4916a0a35d861d
ifzal kola <ifzalkola@gmail.com>
2022-04-02 12:51:35 +0530

    replaced div with an input elecment and made some design changes

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -4,18 +4,34 @@ import { getAlphabets, getNumbers, shuffle } from '../../helpers';
 
 import './Keyboard.css';
 
-const Keyboard = ({ setTextInput, textInput }) => {
+const Keyboard = ({ setTextInput }) => {
   const numbers = getNumbers();
+  // Initial the keyboard with qwerty layout
   const [alphabets, setAlphabets] = useState(getAlphabets());
   const [caps, setCaps] = useState(false);
   const [shift, setShift] = useState(false);
+  /** Handler function for a click on Alphabetical key
+   *
+   * @param key - Alphabetical key that was clicked
+   */
   const handleAlphabetClick = (key) => {
     setTextInput((input) => input + key);
     if (shift) {
       setShift(false);
     }
+    // Reshuffle the letters in the keyboard
     const shuffled = shuffle(getAlphabets());
     setAlphabets(shuffled);
+  };
+  /** Handler function for a click on Numerical key
+   *
+   * @param key - Numerical key that was clicked
+   */
+  const handleNumberClick = (key) => {
+    setTextInput((input) => input + key);
+    if (shift) {
+      setShift(false);
+    }
   };
   const handleCapsClick = () => {
     setCaps(!caps);
@@ -32,19 +48,30 @@ const Keyboard = ({ setTextInput, textInput }) => {
   return (
     <div className='keyboard'>
       <KeyboardKey value='~' id='tilde' />
-      {numbers.map((number) => (
-        <KeyboardKey value={number} id='number' key={number} />
-      ))}
+      {
+        // Rendering All the numerical keys
+        numbers.map((number) => (
+          <KeyboardKey
+            value={number}
+            id='number'
+            key={number}
+            onClick={handleNumberClick}
+          />
+        ))
+      }
       <KeyboardKey value='Tab' id='tab' />
-      {alphabets.map((letter) => (
-        <KeyboardKey
-          key={letter}
-          value={letter}
-          id='alpha'
-          onClick={handleAlphabetClick}
-          caps={shift || caps}
-        />
-      ))}
+      {
+        // Rendering All the alphabetical keys
+        alphabets.map((letter) => (
+          <KeyboardKey
+            key={letter}
+            value={letter}
+            id='alpha'
+            onClick={handleAlphabetClick}
+            caps={shift || caps}
+          />
+        ))
+      }
       <KeyboardKey value='Caps Lock' id='caps' onClick={handleCapsClick} />
       <KeyboardKey value='Space' id='space' onClick={handleSpaceClick} />
       <KeyboardKey value='Shift' id='shift' onClick={handleShiftClick} />
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import './TextInput.css';
 
 const TextInput = ({ value = '' }) => {
-  return <div id='text-input'>{value}</div>;
+  /** Ref for text input */
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // Focus the input component if there is a change in value ie a key was pressed
+    inputRef.current.focus();
+  }, [value]);
+
+  return <input ref={inputRef} id='text-input' value={value}></input>;
 };
 
 export default TextInput;
